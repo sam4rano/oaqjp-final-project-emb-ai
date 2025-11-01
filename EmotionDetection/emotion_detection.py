9227e64bf694cb152e11739d42ec837b541a20f5
Sam4rano <oyerindesamuelabiodun@gmail.com>
2025-11-01 18:22:31 -0400

    new update

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,37 @@
+import requests 
+import json
+
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json=myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    emotion_predictions = formatted_response['emotionPredictions']
+    emotion_data = emotion_predictions[0]['emotion']
+    
+    anger_score = emotion_data['anger']
+    disgust_score = emotion_data['disgust']
+    fear_score = emotion_data['fear']
+    joy_score = emotion_data['joy']
+    sadness_score = emotion_data['sadness']
+    
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
